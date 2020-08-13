3f4c2a121b066ebebb893ce2fbf0824eeba81450
tahscenery <tahscenery@gmail.com>
2020-08-13 18:46:00 +1000

    Added code excerpt

---
+++ greeting.ts
@@ -0,0 +1,7 @@
+let name = "Ta-Seen Islam"
+let description = "an undergraduate student and tech enthusiast"
+let major = "Software Engineering"
+let university = "University of Technology Sydney"
+
+console.log(`Hi! My name is ${name}. I'm ${description} studying ${major} at the ${university}.`)
+// Hi! My name is Ta-Seen Islam. I'm an undergraduate student and tech enthusiast studying Software Engineering at the University of Technology Sydney.
